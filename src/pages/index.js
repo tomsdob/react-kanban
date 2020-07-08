6211a0ae4194b79ce4ebeec0bf0e62a9d830bfca
Toms D <tomy22783@gmail.com>
2020-07-08 15:06:20 +0300

    Drag and drop sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Board from "../components/Board/index";
 
 export default () => {
   return (
     <>
       <Navbar></Navbar>
+      <Board></Board>
     </>
   );
 };
